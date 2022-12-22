8bc02823579bcd5938afe21f35a0c0d7bd6e6f86
Kseniia Prytkova <prytkovakseniia@gmail.com>
2022-12-22 20:53:07 +0000

    add search

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Details from './Details';
+import SearchParams from './SearchParams';
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: Infinity,
+            cacheTime: Infinity
+        }
+    }
+});
 
 const App = () => {
-    return <div className="m-4 text-3xl">hello</div>;
+    return (
+        <div className="m-0 box-border p-0">
+            <BrowserRouter>
+                <QueryClientProvider client={queryClient}>
+                    <header className="flex p-4 text-2xl">
+                        <Link to="/">
+                            <span className="text-4xl">🐶</span> Adopt Them!
+                        </Link>
+                    </header>
+                    <Routes>
+                        <Route path="/details/:id" element={<Details />} />
+                        <Route path="/" element={<SearchParams />} />
+                    </Routes>
+                </QueryClientProvider>
+            </BrowserRouter>
+        </div>
+    );
 };
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
